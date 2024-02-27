12ee393438984acd9757485eb823d831488ae4d4
Cannigia Laluw <cannigialaluw@gmail.com>
2024-02-27 12:24:20 -0800

    added styles to active link in navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,14 @@
-import Link from "next/link";
+"use client";
+
 import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
 import { BsBugFill } from "react-icons/bs";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+  console.log(currentPath);
   const Links = [
     { href: "/", label: "Dashboard" },
     { href: "/issues", label: "Issues" },
@@ -16,7 +22,14 @@ const NavBar = () => {
       <ul className="flex space-x-6">
         {Links.map(({ href, label }) => (
           <li key={`${href}${label}`}>
-            <Link className="text-slate-500 hover:text-slate-800" href={href}>
+            <Link
+              className={classnames({
+                "text-slate-800": href === currentPath,
+                "text-gray-400": href !== currentPath,
+                "hover:text-slate-800 transition-colors": true,
+              })}
+              href={href}
+            >
               {label}
             </Link>
           </li>
